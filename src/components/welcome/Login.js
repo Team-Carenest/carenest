2d7e26d622335cbe1063ec2edbfd7603af10d9a2
Mikayla Dobson <93477693+innocuous-symmetry@users.noreply.github.com>
2022-03-12 16:24:15 -0600

    user login prototyped

--- src/components/welcome/Login.js
+++ src/components/welcome/Login.js
@@ -1,7 +1,48 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
+
 export default function Login() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+
+    })
+
+    const handleLogin = () => {
+        if (!username || !password) {
+            return;
+        }
+
+        if (username && password) {
+            console.log('you can login!');
+        }
+    }
+
+    const handleNav = () => {
+        navigate('/');
+    }
+
     return (
         <>
-        <h1>Login Page</h1>
+        <h1 className="login-header">Please enter your login details:</h1>
+        <form className="login-form">
+            <div className="form-row">
+                <label htmlFor="username">Username: </label>
+                <input type="text" onChange={(e) => setUsername(e.target.value)} required></input>
+            </div>
+
+            <div className="form-row">
+                <label htmlFor="password">Password: </label>
+                <input type="password" onChange={(e) => setPassword(e.target.value)} required></input>
+            </div>
+
+            <button className="button-style-1" onClick={handleLogin}>Login</button>
+
+            <button className="back-button" onClick={handleNav}>Back</button>
+        </form>
         </>
     )
 }
