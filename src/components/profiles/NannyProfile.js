d092b6b2be60849d423a2247cc0fb9653ff4f693
Mikayla Dobson <93477693+innocuous-symmetry@users.noreply.github.com>
2022-03-13 11:48:51 -0500

    updated nannyprofile.js

--- src/components/profiles/NannyProfile.js
+++ src/components/profiles/NannyProfile.js
@@ -4,19 +4,19 @@ import { useNavigate } from "react-router";
 export default function NannyProfile() {
     const navigate = useNavigate();
 
-    const name = store.parentProfiles[0].name;
-    const src = require(`../../media/${store.parentProfiles[0].profileImage}`);
+    const name = store.nannyProfiles[0].name;
+    const src = require(`../../media/${store.nannyProfiles[0].profileImage}`);
     
     const navToCalendar = () => {
         navigate('/nanny/maria/schedule')
     }
 
     return (
-        <div className="parent-profile">
+        <div className="nanny-profile">
             <img src={src} alt={name} />
             <h1>{name}</h1>
 
-            <p>{store.parentProfiles[0].longBio}</p>
+            <p>{store.nannyProfiles[0].longBio}</p>
 
             <button onClick={navToCalendar}>Show current schedule</button>
         </div>
